f4f590245b782e0a0a4423fb5677d289ae9c9962
ajitpande1234 <ajitpande11@gmail.com>
2023-01-22 18:09:23 +0530

    added docstring in app config file

--- store/apps.py
+++ store/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class StoreConfig(AppConfig):
+    """This is for app configuration"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
